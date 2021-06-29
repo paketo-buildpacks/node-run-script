97b4e69fcfc9a2c7ebe317a72234041253a7fa34
AccraZed <acastilla@vmware.com>
2021-06-29 16:18:37 -0400

    add api for buildplan
    changes requirement based on whether a yarn.lock exists
    
    closes #1
    
    Co-authored-by: bhh1893 <bhh1893@users.noreply.github.com>

--- detect.go
+++ detect.go
@@ -1,9 +1,27 @@
 package noderunscript
 
-import "github.com/paketo-buildpacks/packit"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/paketo-buildpacks/packit"
+)
 
 func Detect() packit.DetectFunc {
 	return func(context packit.DetectContext) (packit.DetectResult, error) {
-		return packit.DetectResult{}, nil
+
+		lockName := "npm"
+		if _, err := os.Stat(filepath.Join(context.WorkingDir, "yarn.lock")); err == nil {
+			lockName = "yarn"
+		}
+
+		return packit.DetectResult{
+			Plan: packit.BuildPlan{
+				Requires: []packit.BuildPlanRequirement{
+					{Name: "node"}, {Name: lockName},
+				},
+			},
+		}, nil
+
 	}
 }
--- detect_test.go
+++ detect_test.go
@@ -1,11 +1,13 @@
 package noderunscript_test
 
 import (
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
-	"github.com/paketo-buildpacks/packit"
 	noderunscript "github.com/accrazed/node-run-script"
+	"github.com/paketo-buildpacks/packit"
 	"github.com/sclevine/spec"
 
 	. "github.com/onsi/gomega"
@@ -31,13 +33,34 @@ func testDetect(t *testing.T, context spec.G, it spec.S) {
 		Expect(os.RemoveAll(workingDir)).To(Succeed())
 	})
 
-	context("when conditions for detect true are met", func() {
-		it("detects", func() {
+	context("when the working-dir contains yarn.lock", func() {
+		it("returns a plan that requires node and yarn", func() {
+			Expect(ioutil.WriteFile(filepath.Join(workingDir, "yarn.lock"), nil, 0644)).To(Succeed())
+
 			result, err := detect(packit.DetectContext{
 				WorkingDir: workingDir,
 			})
 			Expect(err).NotTo(HaveOccurred())
-			Expect(result.Plan).To(Equal(packit.BuildPlan{}))
+			Expect(result.Plan).To(Equal(packit.BuildPlan{
+				Requires: []packit.BuildPlanRequirement{
+					{Name: "node"}, {Name: "yarn"},
+				},
+			}))
 		})
 	})
+
+	context("when the working-dir doesn't contain yarn.lock", func() {
+		it("defaults to npm and returns a plan that requires node and npm", func() {
+			result, err := detect(packit.DetectContext{
+				WorkingDir: "/working-dir",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result.Plan).To(Equal(packit.BuildPlan{
+				Requires: []packit.BuildPlanRequirement{
+					{Name: "node"}, {Name: "npm"},
+				},
+			}))
+		})
+	})
+
 }
